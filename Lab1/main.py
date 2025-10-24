fcfe1bed573b6f39110b2fd72c8253c6e0d14408
Mostafa Abayazeed <mostafaabayazeed@yahoo.com>
2025-10-24 08:18:50 -0700

    gui added

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,32 +1,11 @@
-from Lab1.idfs import IDFS
-from puzzle import Puzzle
-from dfs import DFS
-from bfs import BFS
-from astar import AStar
+from gui import PuzzleGUI
+import tkinter as tk
 
 def main():
-    initial = 125340678
-    puzzle = Puzzle(initial)
+    root = tk.Tk()
+    PuzzleGUI(root)
+    root.mainloop()
 
-    print("\n--- Depth-First Search ---")
-    dfs_solver = DFS()
-    dfs_solver.dfs(puzzle)
-
-    print("\n--- Breadth-First Search ---")
-    bfs_solver = BFS()
-    bfs_solver.bfs(puzzle)
-
-    print("\n--- Iterative Search ---")
-    idfs_solver = IDFS()
-    idfs_solver.idfs(puzzle)
-
-    print("\n--- A* (Manhattan) ---")
-    astar_manhattan = AStar("manhattan")
-    astar_manhattan.a_star(puzzle)
-
-    print("\n--- A* (Euclidean) ---")
-    astar_euclidean = AStar("euclidean")
-    astar_euclidean.a_star(puzzle)
 
 if __name__ == "__main__":
     main()
